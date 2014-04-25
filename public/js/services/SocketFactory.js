b06e024a53ff1d9d352dda0614056fec0f58c24d
Noel Madali <amadali@gmail.com>
2014-04-25 15:41:41 -0400

    changed to / for relative url

--- public/js/services/SocketFactory.js
+++ public/js/services/SocketFactory.js
@@ -4,14 +4,14 @@ define([
 ], function(serviceModule, io) {
 	"use strict";
 	return serviceModule.factory("SocketFactory", function($rootScope, $window) {
-		var socket = io.connect("http://home.roastlechon.com:3000?token=" + $window.sessionStorage._id + ":" + $window.sessionStorage.expires + ":" + $window.sessionStorage.token, {
+		var socket = io.connect("/?token=" + $window.sessionStorage._id + ":" + $window.sessionStorage.expires + ":" + $window.sessionStorage.token, {
 			"force new connection" : true,
 			"sync disconnect on unload" : true
 		});
 		return {
 			reconnect: function() {
 				console.log("reconnecting to socket");
-				socket = io.connect("http://home.roastlechon.com:3000?token=" + $window.sessionStorage._id + ":" + $window.sessionStorage.expires + ":" + $window.sessionStorage.token, {
+				socket = io.connect("/?token=" + $window.sessionStorage._id + ":" + $window.sessionStorage.expires + ":" + $window.sessionStorage.token, {
 					"force new connection" : true,
 					"sync disconnect on unload" : true
 				});
